554f173eb414ff87e4c763bb052267c04530fd38
mimimizuno <sukfishdry1217@gmail.com>
2024-09-14 02:09:15 +0900

    Add mkdir sockets

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,11 @@ threads min_threads_count, max_threads_count
 #port        ENV.fetch("PORT") { 3000 }
 bind "unix:///var/www/Todoapp/tmp/sockets/puma.sock"
 
+# ディレクトリが存在しない場合は、作成する
+on_worker_boot do
+    Dir.mkdir("/var/www/Todoapp/tmp/sockets") unless File.exist?("/var/www/Todoapp/tmp/sockets")
+end
+
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
